7daecca413426600c5536a9f4ee6f58441a248e4
finalwoo <finalwoo@ddnayo.com>
2019-10-01 13:30:42 +0900

    Updates

---
+++ precache-manifest.4e9eaf1a3fafe6e8b37ac80ccb5ce9ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea9c239e93aa23a39cc63847a6f525a",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "1ac0e52b806154ac66a9",
+    "url": "/movie_app/static/css/main.29b230ae.chunk.css"
+  },
+  {
+    "revision": "7c246858aa29d751d58c",
+    "url": "/movie_app/static/js/2.1f468cea.chunk.js"
+  },
+  {
+    "revision": "1ac0e52b806154ac66a9",
+    "url": "/movie_app/static/js/main.6f5bb79f.chunk.js"
+  },
+  {
+    "revision": "295510699a648b566819",
+    "url": "/movie_app/static/js/runtime-main.e0aa169c.js"
+  }
+]);
